cb1b0bd2e5c028a5a1ad6be356842c3c7c2e0dbe
neabparinya11 <neabparinya222@gmail.com>
2021-01-19 17:15:53 +0700

    complete

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -21,3 +21,27 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+void showImage(const bool image[][M]){
+	cout<<"------------------------------------------------------------------------\n";
+	for(int row=0;row<30;row++){
+		cout<<"|";
+		for(int colum=0;colum<70;colum++){
+			if(image[row][colum]){
+				cout<<"*";
+			}else{
+				cout<<" ";
+			}
+		}
+		cout<<"|\n";
+	}
+	cout<<"------------------------------------------------------------------------\n";
+}
+void updateImage(bool image[][M],int s,int x,int y){
+	for(int row=0;row<30;row++){
+		for(int colum=0;colum<70;colum++){
+			if(sqrt(pow(row-x,2)+pow(colum-y,2))<=s-1){
+				image[row][colum]=1;
+			}
+		}
+	}
+}
